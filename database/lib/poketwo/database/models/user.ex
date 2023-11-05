2ee64434a5fa6b7c71e9dc39803c62c1ff3c14c4
hope <76099824+HOPE-NEXUS@users.noreply.github.com>
2023-11-05 19:42:57 +0100

    Create user.ex

---
+++ database/lib/poketwo/database/models/user.ex
@@ -0,0 +1,34 @@
+defmodule Poketwo.Database.Models.User do
+  use Ecto.Schema
+  import Ecto.Query
+  alias Poketwo.Database.{Models, V1}
+
+  schema "users" do
+    timestamps(type: :utc_datetime)
+
+    has_many :pokemon, Models.Pokemon
+    has_many :pokedex_entries, Models.PokedexEntry
+  end
+
+  def changeset(user, params \\ %{}) do
+    user
+    |> Ecto.Changeset.cast(params, [:id])
+    |> Ecto.Changeset.unique_constraint(:id, name: :users_pkey)
+  end
+
+  @spec query([{:id, integer}]) :: Ecto.Query.t()
+  def query(id: id) do
+    from u in Models.User,
+      where: u.id == ^id
+  end
+
+  def to_protobuf(%Models.User{} = user) do
+    V1.User.new(
+      id: user.id,
+      inserted_at: user.inserted_at,
+      updated_at: user.updated_at
+    )
+  end
+
+  def to_protobuf(_), do: nil
+end
