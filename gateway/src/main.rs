104fa20e9e371dd9acb44673f28e8f3d31f53769
hope <76099824+HOPE-NEXUS@users.noreply.github.com>
2023-11-05 18:59:37 +0100

    Create main.rs

---
+++ gateway/src/main.rs
@@ -0,0 +1,59 @@
+mod amqp;
+mod config;
+mod gateway;
+
+use anyhow::Result;
+use futures_util::StreamExt;
+use tracing::{info, warn};
+use twilight_gateway::Event;
+use twilight_model::application::interaction::Interaction;
+
+use crate::amqp::Amqp;
+use crate::config::CONFIG;
+use crate::gateway::Gateway;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    tracing_subscriber::fmt::init();
+
+    let amqp = Amqp::connect(&CONFIG).await?;
+    info!("Connected to AMQP");
+
+    let mut gateway = Gateway::connect(&CONFIG).await?;
+    info!("Connected to gateway");
+
+    while let Some(event) = gateway.events.next().await {
+        gateway.cache.update(&event);
+
+        if let Event::Ready(data) = &event {
+            info!("Logged in as {}#{}", data.user.name, data.user.discriminator);
+        }
+
+        if let Some((payload, routing_key)) = get_payload(&event) {
+            if let Err(x) = amqp.publish(&payload, &routing_key).await {
+                warn!("{:?}", x);
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn get_payload(event: &Event) -> Option<(Vec<u8>, String)> {
+    match event {
+        Event::MessageCreate(data) => {
+            Some((serde_json::to_vec(data).ok()?, "MESSAGE_CREATE".into()))
+        }
+        Event::InteractionCreate(data) => match &**data {
+            Interaction::ApplicationCommand(interaction) => Some((
+                serde_json::to_vec(data).ok()?,
+                format!("INTERACTION.APPLICATION_COMMAND.{}", interaction.data.name),
+            )),
+            Interaction::MessageComponent(_) => {
+                Some((serde_json::to_vec(data).ok()?, "INTERACTION.MESSAGE_COMPONENT".into()))
+            }
+            _ => None,
+        },
+        _ => None,
+    }
+}
