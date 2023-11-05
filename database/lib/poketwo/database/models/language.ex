46d127f04e842c141e25e45d65ac9627ffbd97bb
hope <76099824+HOPE-NEXUS@users.noreply.github.com>
2023-11-05 19:24:46 +0100

    Create language.ex

---
+++ database/lib/poketwo/database/models/language.ex
@@ -0,0 +1,28 @@
+defmodule Poketwo.Database.Models.Language do
+  use Ecto.Schema
+  alias Poketwo.Database.{Models, V1}
+
+  schema "languages" do
+    field :iso639, :string
+    field :iso3166, :string
+    field :identifier, :string
+    field :official, :boolean
+    field :order, :integer
+    field :enabled, :boolean
+  end
+
+  @spec to_protobuf(any) :: V1.Language.t() | nil
+  def to_protobuf(_)
+
+  def to_protobuf(%Models.Language{} = language) do
+    V1.Language.new(
+      id: language.id,
+      iso639: language.iso639,
+      iso3166: language.iso3166,
+      identifier: language.identifier,
+      official: language.official
+    )
+  end
+
+  def to_protobuf(_), do: nil
+end
