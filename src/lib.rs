9ff1fc2ffc6e6def05b403fcd82742d45542d85e
hope <76099824+HOPE-NEXUS@users.noreply.github.com>
2023-11-05 19:00:43 +0100

    Create lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,98 @@
+use anyhow::Result;
+use lapin::options::{
+    BasicConsumeOptions, ExchangeDeclareOptions, QueueBindOptions, QueueDeclareOptions,
+};
+use lapin::types::FieldTable;
+use lapin::{Channel, Connection, Consumer, ExchangeKind, Queue};
+use tracing::{debug, info};
+
+static EXCHANGE_DECLARE_OPTIONS: ExchangeDeclareOptions = ExchangeDeclareOptions {
+    passive: false,
+    durable: true,
+    auto_delete: false,
+    internal: false,
+    nowait: false,
+};
+
+static QUEUE_DECLARE_OPTIONS: QueueDeclareOptions = QueueDeclareOptions {
+    passive: false,
+    durable: true,
+    exclusive: false,
+    auto_delete: true,
+    nowait: false,
+};
+
+#[derive(Debug, Clone)]
+pub struct GatewayClientOptions {
+    pub amqp_url: String,
+    pub amqp_exchange: String,
+    pub amqp_queue: String,
+    pub amqp_routing_keys: Vec<String>,
+}
+
+#[derive(Debug)]
+pub struct GatewayClient {
+    pub connection: Connection,
+    pub channel: Channel,
+    pub queue: Queue,
+    pub consumer: Consumer,
+}
+
+impl GatewayClient {
+    pub async fn connect(options: GatewayClientOptions) -> Result<Self> {
+        let connection =
+            lapin::Connection::connect(&options.amqp_url, lapin::ConnectionProperties::default())
+                .await?;
+
+        debug!("Connection established");
+
+        let channel = connection.create_channel().await?;
+
+        debug!("Channel established");
+
+        channel
+            .exchange_declare(
+                &options.amqp_exchange,
+                ExchangeKind::Topic,
+                EXCHANGE_DECLARE_OPTIONS,
+                FieldTable::default(),
+            )
+            .await?;
+
+        debug!("Exchange declared");
+
+        let queue = channel
+            .queue_declare(&options.amqp_queue, QUEUE_DECLARE_OPTIONS, FieldTable::default())
+            .await?;
+
+        debug!("Queue declared");
+
+        let consumer = channel
+            .basic_consume(
+                &options.amqp_queue,
+                "gateway-client",
+                BasicConsumeOptions::default(),
+                FieldTable::default(),
+            )
+            .await?;
+
+        debug!("Consume started");
+
+        for routing_key in &options.amqp_routing_keys {
+            channel
+                .queue_bind(
+                    &options.amqp_queue,
+                    &options.amqp_exchange,
+                    routing_key,
+                    QueueBindOptions::default(),
+                    FieldTable::default(),
+                )
+                .await?;
+        }
+
+        debug!("Queue bind successful");
+        info!("Connected to AMQP");
+
+        Ok(Self { connection, channel, queue, consumer })
+    }
+}
