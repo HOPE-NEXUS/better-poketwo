41b637d571a801a48e6980093e2da78e56c7a321
hope <76099824+HOPE-NEXUS@users.noreply.github.com>
2023-11-05 19:16:26 +0100

    Add grpc endpoint and queries

---
+++ database/config/config.exs
@@ -0,0 +1,17 @@
+import Config
+
+config :poketwo_database, Poketwo.Database.Repo,
+  database: "",
+  username: "",
+  password: "",
+  hostname: ""
+
+config :poketwo_database,
+  ecto_repos: [Poketwo.Database.Repo]
+
+config :grpc,
+  start_server: true
+
+if File.exists?("config/#{Mix.env()}.secret.exs") do
+  import_config("#{Mix.env()}.secret.exs")
+end
