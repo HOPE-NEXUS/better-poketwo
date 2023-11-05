3ac1f6d6acea57808ab2f6e24d66fb5deb6367b3
hope <76099824+HOPE-NEXUS@users.noreply.github.com>
2023-11-05 19:42:28 +0100

    Create type.ex

---
+++ database/lib/poketwo/database/models/type.ex
@@ -0,0 +1,24 @@
+defmodule Poketwo.Database.Models.Type do
+  use Ecto.Schema
+  alias Poketwo.Database.{Models, Utils, V1}
+
+  schema "types" do
+    field :identifier, :string
+
+    has_many :info, Models.TypeInfo
+    belongs_to :generation, Models.Generation
+  end
+
+  @spec to_protobuf(any) :: V1.Type.t() | nil
+  def to_protobuf(_)
+
+  def to_protobuf(%Models.Type{} = type) do
+    V1.Type.new(
+      id: type.id,
+      identifier: type.identifier,
+      info: Utils.map_if_loaded(type.info, &Models.TypeInfo.to_protobuf/1)
+    )
+  end
+
+  def to_protobuf(_), do: nil
+end
