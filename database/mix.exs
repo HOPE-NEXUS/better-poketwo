e294930b9b41a310563d4d54daa7aa47e8c79881
hope <76099824+HOPE-NEXUS@users.noreply.github.com>
2023-11-05 19:11:46 +0100

    Add grpc endpoint and queries

---
+++ database/mix.exs
@@ -0,0 +1,33 @@
+defmodule Poketwo.Database.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :poketwo_database,
+      version: "0.1.0",
+      elixir: "~> 1.13",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {Poketwo.Database.Application, []}
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:poketwo_protobuf, path: "../protobuf-elixir"},
+      {:ecto_sql, "~> 3.7"},
+      {:postgrex, "~> 0.16.2"},
+      {:csv, "~> 2.4"},
+      {:grpc, github: "elixir-grpc/grpc"},
+      {:cowlib, "~> 2.9.0", override: true}
+    ]
+  end
+end
