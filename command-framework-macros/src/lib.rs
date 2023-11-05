06fefa3851b17e98baf3dda4f5caf232b947d772
hope <76099824+HOPE-NEXUS@users.noreply.github.com>
2023-11-05 19:06:14 +0100

    Create lib.rs

---
+++ command-framework-macros/src/lib.rs
@@ -0,0 +1,25 @@
+mod command;
+mod group;
+mod util;
+
+use proc_macro::TokenStream;
+use proc_macro_error::proc_macro_error;
+use syn::{parse_macro_input, AttributeArgs, ItemFn};
+
+#[proc_macro_error]
+#[proc_macro_attribute]
+pub fn command(args: TokenStream, input: TokenStream) -> TokenStream {
+    let args = parse_macro_input!(args as AttributeArgs);
+    let input = parse_macro_input!(input as ItemFn);
+
+    command::command(args, input).into()
+}
+
+#[proc_macro_error]
+#[proc_macro_attribute]
+pub fn group(args: TokenStream, input: TokenStream) -> TokenStream {
+    let args = parse_macro_input!(args as AttributeArgs);
+    let input = parse_macro_input!(input as ItemFn);
+
+    group::group(args, input).into()
+}
